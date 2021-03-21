ba48890b219b01391a6d33081e9b3c206a7b75f9
sofialapteva <sofia_lapteva@mail.ru>
2021-03-21 18:21:40 +0300

    add

---
+++ src/analytics.js
@@ -0,0 +1,21 @@
+function createAnalytics() {
+  let counter = 0;
+  let isDestroyed = false
+
+  const listener = () => counter++
+  document.addEventListener('click', listener)
+
+  return {
+    destroy() {
+      document.removeEventListener('click', listener)
+      isDestroyed = true
+    },
+
+    getClicks() {
+      if (isDestroyed) {
+        console.log('Analytics were destroyed.');
+      }
+      return counter
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Post from './Post'
+const post = new Post('webpack')
+
+console.log(post.toString());
